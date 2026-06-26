27885a63a79139954ae69ef2559f3844178b15e5
kaartiiik <kgupta@trueigtech.com>
2026-06-26 12:55:06 +0530

    this is my firstt commit into rest-api repo

---
+++ data/product.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 60000,
+  },
+  {
+    id: 2,
+    name: "Phone",
+    price: 25000,
+  },
+  {
+    id: 3,
+    name: "Keyboard",
+    price: 1500,
+  },
+];
+
+module.exports = products;
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express() ;
+const products = require("./data/product");
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("This is the root page") ;
+}); 
+
+app.get("/products",(req,res)=>{
+    res.send(products);
+});
+
+
+app.get("/products/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const product = products.find((p)=>p.id === id);
+    if(!product){
+        return res.status(404).json({
+            message : "No product exist with this id"
+        })
+    }
+    res.status(200).json(product);
+});
+
+
+app.post("/products",(req,res)=>{
+    const newproduct  = req.body ; 
+    if(!newproduct){
+        return res.status(400).json({message : "no product in body"});
+    }
+    products.push(newproduct);
+     res.send(products);
+    return res.status(201).json({message:"Product added"})
+    
+})
+
+
+app.listen(8080 , ()=>{
+    console.log("port is running on 8080");
+}); 
